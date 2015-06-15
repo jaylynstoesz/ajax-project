9c853179aa962e12ecc15facb3b6a2b623d106cb
Jaylyn Stoesz <jaylynstoesz@gmail.com>
2015-06-15 10:39:13 -0600

    index complete and formatted

--- js/app-2.js
+++ js/app-2.js
@@ -1,3 +1,5 @@
+var loc = document.location.search;
+
 function parse () {
   var output = {};
   var replaceChars = loc.replace("?", "");
@@ -7,12 +9,11 @@ function parse () {
       var key = pair[0];
       var value = pair[1];
       output[key] = value;
-    };
-    output["urlName"] = name;
+    }
+    output.urlName = name;
   return output;
-};
+}
 
-var loc = document.location.search;
 var site = parse(loc);
 
 var xhr = new XMLHttpRequest();
@@ -21,16 +22,54 @@ xhr.open("GET", "http://www.reddit.com/r/martialarts/comments/" + site.id + "/.j
 xhr.addEventListener("load", function() {
   var doc = JSON.parse(xhr.response);
   var content = document.getElementById("content-main");
-    for (var i = 0; i < 1; i++) {
 
+
+
+
+
+
+    for (var i = 0; i < 1; i++) {
     var preStuff = doc[0].data.children[0].data;
     var titles = preStuff.title;
     var header = document.getElementById("header");
     header.innerHTML = titles;
-  };
+    content.innerHTML = preStuff.selftext;
+  }
+
+  var commentsSecLength = doc[1].data.children.length;
+  for (var j = 0; j < commentsSecLength; j++) {
+    var comments = doc[1].data.children[j].data.body;
+
+    var newDiv = document.createElement("div");
+    var newDivCon = document.createTextNode(comments);
+    newDiv.appendChild(newDivCon);
+    var commentsSection = document.getElementById("comments-section");
+    document.body.insertBefore(newDiv, commentsSection);
+    console.log(commentsSection);
+
+  }
+  var vidSource = doc[0].data.children[0].data.url;
+  var parsedVid = vidSource.split("v=")[1];
+  console.log(parsedVid);
 
 
 
+
+
+
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/player_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  var player;
+  function onYouTubePlayerAPIReady() {
+    player = new YT.Player('ytplayer', {
+      height: '390',
+      width: '640',
+      videoId: parsedVid,
+    });
+  }
 });
 
 xhr.send(null);
--- js/app.js
+++ js/app.js
@@ -1,34 +1,39 @@
-var button = document.getElementById("button");
-
 var xhr = new XMLHttpRequest("button");
 
 xhr.open("GET", "http://www.reddit.com/r/MartialArts.json", true);
 xhr.addEventListener("load", function() {
   var doc = JSON.parse(xhr.response);
-  var content = document.getElementById("content-main")
-  var preStuff = doc.data.children;
 
-    for (var i = 0; i <= preStuff.length; i++) {
-      var id = preStuff[i].data.id;
+  var content = document.getElementById("content-main");
+  var setPath = doc.data.children;
+
+    for (var i = 0; i <= setPath.length; i++) {
+      var id = setPath[i].data.id;
       var ref = "/show.html?id=" + id + "/";
 
-      var titles = preStuff[i].data.title;
-      var a = document.createElement("a");
-      a.innerHTML = titles;
-      a.href = ref;
-      var score = preStuff[i].data.score;
-      var author = preStuff[i].data.author;
-      var numComments = preStuff[i].data.num_comments;
+      var domain = setPath[i].data.domain;
+      if (domain === "youtube.com") {
+        var title = setPath[i].data.title;
+          if (title.length > 150) {
+             title = title.substr(0, 150) + "...";
+           }
+        var numComments = setPath[i].data.num_comments;
+        var score = setPath[i].data.score;
 
-      var newP = document.createElement("div");
-      newP.appendChild(a);
-      var newContent = document.createElement("div");
-      newContent = document.createTextNode("Posted by: " + author + "  -  Score: " + score + "  -  Comments: " + numComments);
-      newP.appendChild(newContent);
-      document.body.insertBefore(newContent, content);
-      document.body.insertBefore(newP, newContent);
-    };
+        var a = document.createElement("a");
+        a.href = ref;
+          if (numComments === 1) {
+            a.innerHTML = "<b>" + title + "</b>" + "<br><br> (" + numComments + " Comment, " + score + " points)";
+          } else {
+            a.innerHTML = "<b>" + title + "</b>" + "<br><br> (" + numComments + " Comments, " + score + " points)";
+          }
 
+        var tile = document.createElement("div");
+        tile.appendChild(a);
+        tile.className = "tile";
+        document.body.insertBefore(tile, content);
+      }
+    }
 });
 
 xhr.send(null);
